2686db22fbecc48a50fc3de1e9503542f6bcb4a9
swordmaidenisawitch-ui <229119855+swordmaidenisawitch-ui@users.noreply.github.com>
2026-05-13 16:58:11 +0530

    code file was added

---
+++ rockpaperscissors.js
@@ -0,0 +1,5 @@
+ /// three variable, rock, paper, scissor
+ //one input from user, another input from computer(random)
+ //rock wins to scissor but loses to paper
+ // paper wins rock but loses to scissor
+ //scissor wins paper but loses to rock
