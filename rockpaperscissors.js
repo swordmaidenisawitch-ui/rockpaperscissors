8e4c5738b3d420c05889e4d6da6756b2ee052a29
swordmaidenisawitch-ui <229119855+swordmaidenisawitch-ui@users.noreply.github.com>
2026-05-13 21:00:15 +0530

    added the html file

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,22 @@
- /// three variable, rock, paper, scissor
+ 
  //one input from user, another input from computer(random)
+ // get input from computer randomly from rock paper scissor
+ //get input from user
+// same timing
+
  //rock wins to scissor but loses to paper
  // paper wins rock but loses to scissor
  //scissor wins paper but loses to rock
+
+/*let rock = "rock"
+let paper ="paper"=
+let scisssor ="Scissors"
+*/
+//computer randomly take the values from string values and randomly choose from em
+//function  getComputerchoice ()
+
+
+if (i=2 ){
+   let randNum = Math.random();
+   console.log(randNum);
+}
