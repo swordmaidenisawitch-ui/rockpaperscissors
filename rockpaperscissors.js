50121f3be5c80b0d165171b58f923c72c23ab764
swordmaidenisawitch-ui <229119855+swordmaidenisawitch-ui@users.noreply.github.com>
2026-06-07 20:56:04 +0530

    code for one round

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,36 +1,60 @@
  
- //one input from user, another input from computer(random)
- // get input from computer randomly from rock paper scissor
- //get input from user
-// same timing
+ 
 
-const PromptSync = require('prompt-sync')();
+const PromptSync = require('prompt-sync');
+const prompt = PromptSync();
 
+ function gethumanchoice() {
+     let choice = prompt("Your Turn :")
+    return choice ;
+ }
+
+function getcomputerchoice () {
+    
+    let generate = Math.floor(Math.random() * 3 )//, function body:return string values likr rock paper  scissor
+ switch (generate ) {
+    case 0:
+          console.log("Computer: Rock");
+        return "Rock";
+        
+        case 1:
+            console.log("Computer: Paper");
+            return "Paper";
+         
+            case 2:
+                console.log("Computer: Scissor");
+                return "Scissor";
+            
+    }
+    
+}
+
+let humanScore  = 0;
+let computerScore = 0;
 
 
  
- function gethumanchoice() {
-      let promptchoice = PromptSync(' Your Turn : ')
-      return promptchoice;
-}
-console.log(gethumanchoice());
+ function PlayRound(humanChoice, computerChoice) {
+ 
+  
+    if (
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Scissor" && computerChoice === "Paper") ||
+        (humanChoice === "Rock" && computerChoice === "Scissor")
+    ) {
+        console.log("You win!");
+        humanScore++;
 
-function getcomputerchoice () {
-    let generate = Math.floor(Math.random() * 3 )//, function body:return string values likr rock paper  scissor
-    switch (generate) {
-    case 0:
-        return "Rock";
-        break;
-        case 1:
-            return "Paper";
-            break;
-            case 2:
-                return "Scissor";
-                break;
-     
+    } else if (humanChoice === computerChoice) {
+        console.log("It's a Tie");
+    
+    } else {
+        console.log("You lose, Computer wins");
+        computerScore++;
     }
-      
+    
+    
+
 }
-console.log(getcomputerchoice());
-
-
+PlayRound(gethumanchoice(), getcomputerchoice());
+console.log(humanScore, computerScore);
